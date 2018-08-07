e12c0510514a4df8fea1f11a3fe9cb0607d8412c
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-07 04:08:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,6 +28,12 @@ class App < Sinatra::Base
     "#{@word1 @word2 @word3 @word4 @word5}"
   end
   
+  get '/:divide/:number1/:number2' do
+    @divide = params[:divide]
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    "#{@number1.to_i/@number2.to_i}"
+  end
   
 
 end
