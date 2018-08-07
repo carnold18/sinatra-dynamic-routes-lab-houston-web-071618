ec7cca4a26777058ce41916b9bc17283b4b2d9c7
Christopher Arnold <chrisarnold2010@gmail.com>
2018-08-07 04:21:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1]
     @number2 = params[:number2]
-    if @operation == "add"
+    if @operation == :add
     "#{@number1.to_i+@number2.to_i}"
     elsif @operation == "subtract"
     "#{@number1.to_i-@number2.to_i}"
